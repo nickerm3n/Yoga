d5fb4cda0548c16cf73b10c3049eec783d8be9cc
Nikita <nickermen13@gmail.com>
2019-04-09 17:42:03 +0300

    add scheldure

--- js/yoga.js
+++ js/yoga.js
@@ -1,14 +1,30 @@
 (function () {
-	document.getElementById('slider').onclick = sliderLeft;
+	document.getElementById('slider-left').onclick = sliderLeft;
+	document.getElementById('slider-right').onclick = sliderRight;
 
 }
 )()
 
-var position = -1065;
+var position = 0  //-1065
 
 function sliderLeft() {
-	var sliderBox = document.querySelector('.slider-box');	
-	sliderBox.style.left = position + 'px';
-	(position > -2130 ? position += -1065 : position = 0)
+	var sliderBox = document.querySelector('.slider-box');
+	if (position === -1065) {
+		position = 0
+		sliderBox.style.left = position + 'px';
+	}
+	if (position === -2130) {
+		position = 1065;
+		sliderBox.style.left = position + 'px';
+	}
 
+
+	// sliderBox.style.left = position + 'px';
+	// (position > -2130 ? position += -1065 : position = 0)
+}
+
+function sliderRight() {
+	var sliderBox = document.querySelector('.slider-box');	
+	(position > -2130 ? position += -1065 : position = 0)
+	sliderBox.style.left = position + 'px';
 }
